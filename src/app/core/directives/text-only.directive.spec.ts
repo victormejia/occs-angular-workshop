2241955d9fee6f6ef01441a52419723a92e9e566
Victor Mejia <victor.a.mejia@gmail.com>
2017-08-16 11:12:13 -0700

    TextOnlyDirective completed

---
+++ src/app/core/directives/text-only.directive.spec.ts
@@ -0,0 +1,8 @@
+import { TextOnlyDirective } from './text-only.directive';
+
+describe('TextOnlyDirective', () => {
+  it('should create an instance', () => {
+    const directive = new TextOnlyDirective();
+    expect(directive).toBeTruthy();
+  });
+});
