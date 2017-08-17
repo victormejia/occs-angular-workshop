22a0efef24ce329a542241f81b84b4a99a1b4fa8
Victor Mejia <victor.a.mejia@gmail.com>
2017-08-17 10:46:22 -0700

    HackerService unit tests

--- src/app/core/services/hacker.service.spec.ts
+++ src/app/core/services/hacker.service.spec.ts
@@ -1,15 +1,43 @@
-import { TestBed, inject } from '@angular/core/testing';
-
 import { HackerService } from './hacker.service';
+import { mockHackers } from '../helpers.spec';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
-describe('HackerService', () => {
+fdescribe('HackerService', () => {
+  let service: HackerService;
+  const httpSpy = jasmine.createSpyObj('http', ['get']);
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [HackerService]
+    service = new HackerService(httpSpy);
+  });
+
+  describe('getHackers', () => {
+
+    it('should return list of hackers', (done) => {
+      const mockResponse = Observable.of(mockHackers);
+
+      httpSpy.get.and.returnValue(mockResponse);
+
+      service.getHackers()
+        .subscribe(data => {
+          expect(httpSpy.get).toHaveBeenCalledWith('/api/hackers?q=');
+          expect(data).toEqual(mockHackers);
+          done();
+        });
+    });
+
+    it('should return list of hackers when called with search term', (done) => {
+      const mockResponse = Observable.of(mockHackers);
+
+      httpSpy.get.and.returnValue(mockResponse);
+
+      service.getHackers('Ig')
+        .subscribe(data => {
+          expect(httpSpy.get).toHaveBeenCalledWith('/api/hackers?q=Ig');
+          expect(data).toEqual(mockHackers);
+          done();
+        });
     });
   });
 
-  it('should be created', inject([HackerService], (service: HackerService) => {
-    expect(service).toBeTruthy();
-  }));
 });
